72118809b8f03a5448e82ba522e80674d9211b24
CJ Horlebein <chrishorlebein@gmail.com>
2019-01-15 21:40:30 -0500

    working heroku DB

--- config/database.js
+++ config/database.js
@@ -5,19 +5,19 @@ module.exports = new Sequelize(
   'dpodsthlznztic', //username
   '5fc7c2d864282715601bb38851c4ff2e30e4b9adc944566ea8e5ee3ed7d9e33e', //password
   {
-    host: 'ec2-107-22-162-8.compute-1.amazonaws.comt',
+    host: 'ec2-107-22-162-8.compute-1.amazonaws.com',
     dialect: 'postgres',
     protocol: 'postgres',
-    port: "5432",
+    port: '5432',
     operatorsAliases: false,
     dialectOptions: {
       ssl: true
     },
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     }
   }
 );
